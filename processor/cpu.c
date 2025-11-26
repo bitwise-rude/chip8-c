de73bd434c63d4579bda444ed55c89a82d383766
bitwise-rude <080bel042.meyan@pcampus.edu.np>
2025-11-26 17:14:01 +0545

    filled opcodes

--- processor/cpu.c
+++ processor/cpu.c
@@ -10,7 +10,22 @@ CPU *make_cpu(struct Memory *mem){
 }
 
 void fill_opcode(Opcode *op_s, u8 *op_v){
+    // TODO; some redundant values will be removed later
+    op_s->msb = *op_v;
+    op_s->lsb = *(op_v+1);
 
+    op_s->opcode = combine_bytes(op_s->msb,op_s->lsb);
+    op_s->nnn = op_s->opcode & (0x0FFF);
+    op_s->n = op_s->lsb & 0x0F;
+    op_s->x = op_s->msb & 0x0F;
+    op_s->y = op_s->lsb >> 4 ;
+    op_s->kk = op_s->lsb;
+
+
+}
+
+u16 combine_bytes(u8 msb, u8 lsb){
+    return  (((u16)msb<<8 )| (u16)lsb);
 }
 
 
@@ -20,7 +35,8 @@ void step(CPU *cpu){
 
     // create the opcode
     Opcode op = (Opcode){};
-    // fill_opcode(&op);
+    fill_opcode(&op,opcode);
+
     printf("THE OPCODE IS %x",*opcode);
 
     
